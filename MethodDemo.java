785a489f21b4d2f5e44c480dcefb687f9a4ba761
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-05-31 08:07:37 +0530

    Create MethodDemo.java

---
+++ MethodDemo.java
@@ -0,0 +1,12 @@
+//code
+class MethodDemo {
+    public void myMethod(){
+        System.out.println("The code is executing using Method");
+    }
+    public static void main(String[] args) {
+       MethodDemo call = new MethodDemo();
+       call.myMethod();
+    }
+}
+
+// output: The code is executing using Method
