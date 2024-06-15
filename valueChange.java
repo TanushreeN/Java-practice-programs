f86c9441e5f6be0b83bd53598d6a8be9abd3fd9b
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-06-15 10:38:25 +0530

    Create valueChange.java

---
+++ valueChange.java
@@ -0,0 +1,9 @@
+class valueChange {
+    int a = 10;
+    public static void main(String[] args) {
+        int a = 12;
+        System.out.println("The value changed to " + a);
+    }
+}
+
+// op: The value changed to 12
