a7888e6f5f036caa376b815a1a0d2e7aad5aa1b8
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-06-22 11:37:52 +0530

    class_subtract.java

---
+++ class_subtract.java
@@ -0,0 +1,17 @@
+class Subtract {
+    static class Calculator{
+        int num1;
+        int num2;
+        public int subtract(int num1, int num2){
+            int result = num1 - num2;
+            return result;
+        }
+    }
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        int subtract = c.subtract(6,4);
+        System.out.println("The subtraction of " + 6 + " and " + 4 + " is: " + subtract);
+    }
+}
+
+//op: The subtraction of 6 and 4 is: 2
