ef7c802fe66477e04e23dca029b1d146e23503d2
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-06-22 11:36:31 +0530

    Update class_multiply.java

--- class_multiply.java
+++ class_multiply.java
@@ -2,14 +2,14 @@ class Multiply {
     static class Calculator{
         int num1;
         int num2;
-        public int add(int num1, int num2){
+        public int multiply(int num1, int num2){
             int result = num1 * num2;
             return result;
         }
     }
     public static void main(String[] args) {
         Calculator c = new Calculator();
-        int multiply = c.add(6,4);
+        int multiply = c.multiply(6,4);
         System.out.println("The Multiplication of " + 6 + " and " + 4 + " is: " + multiply);
     }
 }
