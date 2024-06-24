8c57d812ee6df2d277ca70fb1faf1d4c170f9934
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-06-24 15:53:53 +0530

    calculator.java

---
+++ calculator.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the Num1:");
+        int Num1 = sc.nextInt();
+        
+        System.out.println("Select the operator:");
+        sc.nextLine();
+        String operator = sc.nextLine();
+        
+        System.out.println("Enter the Num2:");
+        int Num2 = sc.nextInt();
+       
+        switch(operator)
+        {
+        case "+": 
+            System.out.println("Result: " +(Num1 + Num2));
+            break;
+        case "-":
+            System.out.println("Result: "+(Num1 - Num2));
+            break;
+        case "*":
+            System.out.println("Result: "+(Num1 * Num2));
+            break;
+        case "/":
+            if(Num2 != 0)
+            {
+                System.out.println("Result: "+(Num1 / Num2));
+            }
+            else{
+                System.out.println("Error !!!, Cannot Divide by zero");
+            }
+            break;
+        default:
+            System.out.println("Ooops....The opration is not taken here");
+        
+        }
+        sc.close();
+    }
+}
+
+// OP:Enter the Num1:
+// 3
+// Select the operator:
+// +
+// Enter the Num2:
+// 5
+// Result: 8
