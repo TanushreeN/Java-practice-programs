47a0898e882a2a06a7fbaa7d6cf6ead285c2ed75
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-06-27 19:30:17 +0530

    Create 2_objs.java

---
+++ 2_objs.java
@@ -0,0 +1,27 @@
+class Mobile{
+    String brand;
+    int price;
+    static String name;
+    
+    public void show() {
+        System.out.println(brand + " : " + price + " : " + name);
+    }
+}
+public class HelloWorld {
+    public static void main(String[] args) {
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Samsung";
+        obj1.price = 20_000;
+        Mobile.name = "phone";
+        
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Apple";
+        obj2.price = 80_000;
+        Mobile.name = "phone";
+        
+        obj1.show();
+        obj2.show();
+    }
+}
+
+//output by creating 2 objects created..
