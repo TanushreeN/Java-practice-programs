26557111dc8195127f3881ad473103c34ac2d863
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-07-02 14:59:36 +0530

    Simple_Inerest.java

---
+++ Simple_Inerest.java
@@ -0,0 +1,14 @@
+class Interest {
+    
+    public int result(float p, float t, float r)
+    {
+        return (int) ((p * t * r)/100);
+    }
+    public static void main(String[] args) {
+        Interest in = new Interest();
+         int Result = in.result(1000, 5, 5);
+        System.out.print(Result);
+    }
+}
+
+// OP : 250
