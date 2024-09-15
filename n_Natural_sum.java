078623d77a24d100c80ee6ea1e02fc303fc19a09
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-09-15 17:28:40 +0530

    Add files via upload

---
+++ n_Natural_sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class n_Natural_sum
+{
+	public static void main(String[] args)
+	{
+		int sum = 0, n=1;
+		System.out.println("Enter the value of n");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+				
+		
+		sum = n * (n+1)/2;
+			
+		System.out.println(sum);
+		
+	}
+}
