c384820a81206e2510378a954e0ca79196208b25
Tanushree N Naik <136624898+TanushreeN@users.noreply.github.com>
2024-11-16 21:36:58 +0530

    MethodDemo.java

--- MethodDemo.java
+++ MethodDemo.java
@@ -5,6 +5,7 @@ class MethodDemo
     public void myMethod(){
         System.out.println("The code is executing using Method");
     }
+
     public static void main(String[] args) {
        MethodDemo call = new MethodDemo();
        call.myMethod();
